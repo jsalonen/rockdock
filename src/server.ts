06ac4ccdf91cfbeeb308fd5ffc26bec83a85b4bf
Jaakko Salonen <jaakko.salonen@iki.fi>
2015-12-06 00:41:50 +0200

    Added skeleton for express server with TypeScript/Webpack

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+///<reference path='../typings/tsd.d.ts' />
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Rockdock');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Rockdock server listening at http://%s:%s', host, port);
+});
